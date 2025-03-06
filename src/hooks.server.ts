bb186b1251902e51f54e8fc7067cf08ed4b5bd55
Leonid <firefoxwolf0824@gmail.com>
2025-03-06 18:21:49 +0700

    checkAuth запрос + проверка защищенных путей

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,20 @@
 import type { Handle } from '@sveltejs/kit';
 import { redirect } from '@sveltejs/kit';
 
+import { checkAuth } from './routes/login/api/checkAuth';
+
 export const handle: Handle = async ({ event, resolve }) => {
 	const theme = event.cookies.get('theme') || 'light';
-	const authToken = event.cookies.get('access_token');
-	console.log(`Acces Token: ${authToken}`);
+	const protectedRoutes = ['/main', '/profile', '/rules'];
+	if (protectedRoutes.includes(event.url.pathname)) {
+		const authResult = await checkAuth();
+		console.log('Результат проверки авторизации:', authResult);
 
-	if (!authToken && event.url.pathname.startsWith('/profile')) {
-		throw redirect(302, '/login');
+		if (!authResult.success) {
+			throw redirect(302, '/login');
+		} else {
+			console.log('Пользователь авторизован. Доступ разрешен.');
+		}
 	}
 
 	const response = await resolve(event, {
