298fa8967611ebb88bb44f5757a62dd0296a4dd6
Leonid <firefoxwolf0824@gmail.com>
2025-03-14 18:42:33 +0700

    components. изменены стили и названия ссылок

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,14 +4,18 @@
 
 <div class="header">
 	<nav class="header__navbar">
-		<a class="header__navbar__link" href="/main" class:active={page.url.pathname === '/main'}
-			>Главная</a
+		<a
+			class="header__navbar__link"
+			href="/search-lobby"
+			class:active={page.url.pathname === '/search-lobby'}>Найти лобби</a
 		>
 		<a class="header__navbar__link" href="/profile" class:active={page.url.pathname === '/profile'}
 			>Профиль</a
 		>
-		<a class="header__navbar__link" href="/rules" class:active={page.url.pathname === '/rules'}
-			>Правила</a
+		<a
+			class="header__navbar__link"
+			href="/create-lobby"
+			class:active={page.url.pathname === '/create-lobby'}>Создать лобби</a
 		>
 	</nav>
 </div>
@@ -19,7 +23,7 @@
 <style lang="scss">
 	.header {
 		display: flex;
-		justify-content: space-between;
+		justify-content: center;
 		gap: 15px;
 		padding: 10px;
 	}
@@ -30,26 +34,6 @@
 		gap: 20px;
 
 		.header__navbar__link {
-			position: relative;
-			text-decoration: none;
-			color: var(--text-color);
-			padding-bottom: 5px;
-
-			&::after {
-				content: '';
-				position: absolute;
-				left: 0;
-				bottom: 0;
-				width: 0;
-				height: 1px;
-				background-color: var(--text-color);
-				transition: width 0.3s ease-in-out;
-			}
-
-			&:hover::after {
-				width: 100%;
-			}
-
 			&.active::after {
 				width: 100%;
 				background-color: var(--text-color);
